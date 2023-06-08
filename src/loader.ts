2726cb7f71763c56182994aff1a0906a30df7081
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2023-06-08 14:17:45 +0200

    Monorepo progress

--- src/loader.ts
+++ src/loader.ts
@@ -1,8 +1,9 @@
 import jscc from "jscc";
 import JsccOptions = jscc.Options;
+import type { LoaderContext } from "webpack";
 
-export default function (source: string, _inputSourceMap: any) {
-    const options: JsccOptions = this.getOptions(this.loaderContext);
+export default function (this: LoaderContext<JsccOptions>, source: string, _inputSourceMap: any): void {
+    const options: JsccOptions = this.getOptions(this);
     const { code, map } = jscc(source, this.resourcePath, options);
     this.callback(null, code, map);
 }
